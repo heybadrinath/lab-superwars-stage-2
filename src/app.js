cf9622376ca466cc08a23901e1949dd7f39cf27d
heybadrinath <ram.r@kalvium.community>
2023-10-05 16:28:03 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -26,7 +26,23 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for(let i=0;i<20;i++){
+        if (i%2==0){
+            detailedPlayers.push({
+                name:players[i],
+                strength:getRandomStrength(),
+                image:`./images/super-${i+1}.png`,
+                type:'hero'
+            })
+        }else{
+            detailedPlayers.push({
+                name:players[i],
+                strength:getRandomStrength(),
+                image:`./images/super-${i+1}.png`,
+                type:'villain'
+            })
+        }  
+    }
     return detailedPlayers;
 }
 
@@ -34,17 +50,30 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*101)
 }
 
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = ''
 
-    // Loop through players and accumulate HTML template
+       // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
 
-    return fragment;
+    for(let i=0;i<20;i++){
+       if(type=="hero"&& players[i].type=="hero"){
+        fragment+=`<div class="player"><img src="${players[i].image}" alt=" "><div class="name">${players[i].name}</div><div class="strength">${players[i].strength}</div></div>`
+       }else if (type=="villain" && players[i].type=="villain"){
+        fragment+=`<div class="player"><img src="${players[i].image}" alt=" "><div class="name">${players[i].name}</div><div class="strength">${players[i].strength}</div></div>`
+       }
+       
+    }
+    console.log(fragment)
+       return fragment;
+
 }
+
+ 
 // Display players in HTML
 const viewPlayers = (players) => {
 
