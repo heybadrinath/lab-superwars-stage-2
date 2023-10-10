cec16578e2bd0cfff8f20342ad301bc4c8ab3046
heybadrinath <ram.r@kalvium.community>
2023-10-10 18:08:30 +0530

    index.html

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ const PLAYERS = [
     "Captain",
     "Catwoman",
     "Fish",
-    "Hulk",
-    "Ninja",
+    "mad Hulk",
+    "Dead Pool",
     "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+    "Evil Strange",
+    "hammer",
+    "She Hulk",
+    "Wasp",
+    "Thanos"
 ];
 
 // initialize players with image and strength
@@ -27,6 +27,24 @@ const initPlayers = (players) => {
     // Create players using for loop
     // Type your code here
 
+    for(let i=1;i<=20;i++){
+        if (i<=10){
+            detailedPlayers.push({
+                name:players[i-1],
+                strength:getRandomStrength(),
+                image:`./images/super-${i}.png`,
+                type:'hero'
+            })
+        }else{
+            detailedPlayers.push({
+                name:players[i-1],
+                strength:getRandomStrength(),
+                image:`./images/super-${i}.png`,
+                type:'villain'
+            })
+        }
+       
+    }
     return detailedPlayers;
 }
 
@@ -37,15 +55,40 @@ const getRandomStrength = () => {
     return Math.ceil(Math.random()*101);
 }
 
-const buildPlayers = (players, type) => {
-    let fragment = '';
 
+const view = (playerObj) => {
+    let player = document.createElement('div');
+    player.classList.add('player');
+    let image = document.createElement('img');
+    image.setAttribute('src', playerObj.image);
+    image.setAttribute('alt', '');
+    let name = document.createElement('div');
+    name.className = 'name';
+    name.textContent = playerObj.name;
+    let strength = document.createElement('div');
+    strength.textContent = playerObj.strength;
+    strength.className = 'strength';
+    player.append(image, name, strength);
+    return player;
+  };
+
+
+const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
-    return fragment;
+    for(let i=0;i<20;i++){
+        if(type=="hero"&& players[i].type=="hero"){
+         
+         fragment=fragment+view(players[i])
+        }else if (type=="villain" && players[i].type=="villain"){
+         fragment=fragment+view(player[i])
+ 
+        }
+    return fragment.innerHTML;
+    }
 }
+
 // Display players in HTML
 const viewPlayers = (players) => {
 
@@ -56,4 +99,6 @@ const viewPlayers = (players) => {
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
+    // console.log(viewPlayers(initPlayers(PLAYERS)))
+    // viewPlayers(buildPlayers(detailedPlayers,detailedPlayers.type));
 }
